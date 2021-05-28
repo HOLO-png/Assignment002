d928a4fbdbe60fee3c94d336e7874bef6bf78a58
HOLO-png <69238651+HOLO-png@users.noreply.github.com>
2021-05-28 15:41:32 +0700

    first commit

--- check.js
+++ check.js
@@ -7,14 +7,12 @@ document.addEventListener(
     const inputForm = $$('input');
     const btn = $('button');
     const print_data = $('.print_data');
+
     var valueBox1 = 0;
     var valueBox2 = 0;
     var valueBox3 = 0;
     var valueBox4 = 0;
-    var arrValueBox1 = [];
-    var arrValueBox2 = [];
-    var arrValueBox3 = [];
-    var arrValueBox4 = [];
+    var arrValueInput = [];
 
     inputForm[0].onchange = function (e) {
       valueBox1 = e.target.value;
@@ -37,44 +35,71 @@ document.addEventListener(
       var sogocconlai;
       var lai;
       var gocLai;
+      var ObjVal = {
+        valueBox1: valueBox4,
+        valueBox2: valueBox1.toLocaleString(),
+        valueBox3: '',
+        valueBox4: '',
+        valueBox5: '',
+      };
+      arrValueInput.push(ObjVal);
       sogoc = valueBox1 / valueBox2;
-      arrValueBox4.push(Math.ceil(sogoc));
+      var tonglai = 0;
+      var tonggoclai = 0;
+
       for (let i = 0; i < valueBox2; i++) {
+        // var date = new Date(valueBox4);
+        // day = date.getDate();
+        // month = date.getMonth();
+        // year = date.getFullYear();
+        // console.log(day);
+        // console.log(month);
+        // console.log(year);
+
+        // if (month < 0) {
+        //   year = date.getFullYear();
+        //   month = 12;
+        // } else if (month > 12) {
+        //   month = 1 + i;
+        //   year = date.getFullYear();
+        // }
+        // valueBox4 = [year, month, day].join('-');
         sogocconlai = valueBox1 - sogoc * (i + 1);
         lai = ((valueBox1 - sogoc * i) * valueBox3) / valueBox2 / 100;
         gocLai = sogoc + lai;
-        arrValueBox3.push(Math.ceil(gocLai));
-        arrValueBox2.push(Math.ceil(lai));
-        arrValueBox1.push(Math.ceil(sogocconlai));
-        inKetQua(arrValueBox1, arrValueBox2, arrValueBox3, arrValueBox4);
+        tonglai += lai;
+        tonggoclai += gocLai;
+        ObjVal = {
+          valueBox1: valueBox4,
+          valueBox2: sogocconlai,
+          valueBox3: sogoc,
+          valueBox4: lai,
+          valueBox5: gocLai,
+        };
+        arrValueInput.push(ObjVal);
       }
-      function inKetQua(
-        arrValueBox1,
-        arrValueBox2,
-        arrValueBox3,
-        arrValueBox4
-      ) {
-        for (let i = 0; i < valueBox2; i++) {
-          const data = `
-                <tr>
-                  ${arrValueBox1.map(
-                    (val) => `<td>${val.toLocaleString()}</td>`
-                  )}
-                  ${arrValueBox2.map(
-                    (val) => `<td>${val.toLocaleString()}</td>`
-                  )}
-                  ${arrValueBox3.map(
-                    (val) => `<td>${val.toLocaleString()}</td>`
-                  )}
-                  ${arrValueBox4.map(
-                    (val) => `<td>${val.toLocaleString()}</td>`
-                  )}
-                </tr>`;
-          print_data.innerHTML = data;
-          console.log(data);
-        }
-      }
+      ObjVal = {
+        valueBox1: 'Tổng:',
+        valueBox2: '',
+        valueBox3: valueBox1.toLocaleString(),
+        valueBox4: tonglai,
+        valueBox5: tonggoclai,
+      };
+      arrValueInput.push(ObjVal);
+      inKetQua(arrValueInput);
     };
+    function inKetQua(arr) {
+      const data = arr.map((val) => {
+        return `<tr>
+                      <td>${val.valueBox1.toLocaleString()}</td>
+                      <td>${val.valueBox2.toLocaleString()}</td>
+                      <td>${val.valueBox3.toLocaleString()}</td>
+                      <td>${val.valueBox4.toLocaleString()}</td>
+                      <td>${val.valueBox5.toLocaleString()}</td>
+                    </tr>`;
+      });
+      print_data.innerHTML = data;
+    }
   },
   false
 );
